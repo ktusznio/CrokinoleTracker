164b35b2954ff4385c877d59cd4c18c59a44e295
Max Woghiren <max.woghiren@gmail.com>
2012-04-24 01:23:10 -0400

    Added methods to determine a player's win and loss counts.

--- CrokinoleTracker/CrokinoleTracker/Player.m
+++ CrokinoleTracker/CrokinoleTracker/Player.m
@@ -6,11 +6,36 @@
 //  Copyright (c) 2012 KMSoft. All rights reserved.
 //
 
+#import "Game.h"
 #import "Player.h"
 
 @implementation Player
 
 @dynamic name;
 @dynamic games;
+
+- (int)wins {
+    int wins = 0;
+
+    for (Game *game in [self games]) {
+        if ([game winningPlayer] == self) {
+            wins++;
+        }
+    }
+
+    return wins;
+}
+
+- (int)losses {
+    int losses = 0;
+
+    for (Game *game in [self games]) {
+        if ([game winningPlayer] != self) {
+            losses++;
+        }
+    }
+
+    return losses;
+}
 
 @end

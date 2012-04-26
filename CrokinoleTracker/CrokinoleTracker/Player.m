b4b862123fe92e0a4a36926d31bc2024a97941e0
Max Woghiren <max.woghiren@gmail.com>
2012-04-26 00:27:58 -0400

    Moved the calculation of player stats to a more appropriate place.

--- CrokinoleTracker/CrokinoleTracker/Player.m
+++ CrokinoleTracker/CrokinoleTracker/Player.m
@@ -17,44 +17,7 @@
 @dynamic games;
 
 - (PlayerStatistics *)statistics {
-    PlayerStatistics *playerStatistics = [[PlayerStatistics alloc] initForPlayer:self];
-
-    int wins = 0;
-    int losses = 0;
-    int totalGames = [[self games] count];
-    int totalRounds = 0;
-    int totalPoints = 0;
-    int totalTwenties = 0;
-
-    for (Game *game in [self games]) {
-        // Increment the appropriate win/loss statistic.
-        if ([game winningPlayer] == self) {
-            wins++;
-        } else {
-            losses++;
-        }
-
-        // Determine the total number of rounds, points, and twenties.
-        for (Round *round in [game rounds]) {
-            totalRounds++;
-            totalPoints += [round scoreForPlayer:self];
-            totalTwenties += [round twentiesForPlayer:self];
-        }
-    }
-
-    double pointsPerGame = (totalGames > 0) ? (totalPoints * 1.0 / totalGames) : 0;
-    double pointsPerRound = (totalRounds > 0) ? (totalPoints * 1.0 / totalRounds) : 0;
-    double twentiesPerGame = (totalGames > 0) ? (totalTwenties * 1.0 / totalGames) : 0;
-    double twentiesPerRound = (totalRounds > 0) ? (totalTwenties * 1.0 / totalRounds) : 0;
-
-    [playerStatistics setWins:wins];
-    [playerStatistics setLosses:losses];
-    [playerStatistics setPointsPerGame:pointsPerGame];
-    [playerStatistics setPointsPerRound:pointsPerRound];
-    [playerStatistics setTwentiesPerGame:twentiesPerGame];
-    [playerStatistics setTwentiesPerRound:twentiesPerRound];
-
-    return playerStatistics;
+    return [[PlayerStatistics alloc] initForPlayer:self];
 }
 
 @end

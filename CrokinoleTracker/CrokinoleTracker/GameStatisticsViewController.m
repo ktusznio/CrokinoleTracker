fe4413bc74a581433c0f317bdfdf2300ae3c4099
Kamil Tusznio <kamil.tusznio@gmail.com>
2012-04-26 21:43:50 -0400

    Added a game statistics screen.

--- CrokinoleTracker/CrokinoleTracker/GameStatisticsViewController.m
+++ CrokinoleTracker/CrokinoleTracker/GameStatisticsViewController.m
@@ -6,10 +6,12 @@
 //  Copyright (c) 2012 KMSoft. All rights reserved.
 //
 
+#import "GameStatisticsViewController.h"
+
 #import "AppDelegate.h"
 #import "CoreDataUtilities.h"
 #import "Game.h"
-#import "GameStatisticsViewController.h"
+#import "GameStatistics.h"
 #import "Player.h"
 
 const int ALERT_VIEW_CANCEL_DELETE_BUTTON_INDEX = 0;
@@ -17,19 +19,78 @@ const int ALERT_VIEW_DELETE_BUTTON_INDEX = 1;
 
 @implementation GameStatisticsViewController
 
+@synthesize playerOneLabel;
+@synthesize playerTwoLabel;
+@synthesize playerOneScoreLabel;
+@synthesize playerTwoScoreLabel;
+@synthesize playerOneRoundsWonLabel;
+@synthesize playerTwoRoundsWonLabel;
+@synthesize playerOnePointsPerRoundLabel;
+@synthesize playerTwoPointsPerRoundLabel;
+@synthesize playerOneTwentiesLabel;
+@synthesize playerTwoTwentiesLabel;
+@synthesize playerOneFifteensLabel;
+@synthesize playerTwoFifteensLabel;
+@synthesize playerOneTensLabel;
+@synthesize playerTwoTensLabel;
+@synthesize playerOneFivesLabel;
+@synthesize playerTwoFivesLabel;
+
 - (id)initForGame:(Game *)aGame {
     self = [super init];
-
+    
     if (self) {
         game = aGame;
-
+        
         // Set the text on the navigation bar.
         Player *playerOne = [[game players] objectAtIndex:0];
         Player *playerTwo = [[game players] objectAtIndex:1];
         [self setTitle:[NSString stringWithFormat:@"%@ vs %@", [playerOne name], [playerTwo name]]];
     }
-
+    
     return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Change the text on the back bar button item.
+    UIBarButtonItem *backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Back"
+                                                                          style:UIBarButtonItemStylePlain
+                                                                         target:nil
+                                                                         action:nil];
+    [[self navigationItem] setBackBarButtonItem:backBarButtonItem];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    // Set the statistics labels.
+    GameStatistics *gameStatistics = [[GameStatistics alloc] initForGame:game];
+    Player *playerOne = [[game players] objectAtIndex:0];
+    Player *playerTwo = [[game players] objectAtIndex:1];
+    
+    [playerOneLabel setText:[playerOne name]];
+    [playerTwoLabel setText:[playerTwo name]];
+    
+    [playerOneScoreLabel setText:[NSString stringWithFormat:@"%d", [game playerOneScore]]];
+    [playerTwoScoreLabel setText:[NSString stringWithFormat:@"%d", [game playerTwoScore]]];
+    
+    [playerOneRoundsWonLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics roundsWonForPlayer:playerOne]]];
+    [playerTwoRoundsWonLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics roundsWonForPlayer:playerTwo]]];
+    
+    [playerOnePointsPerRoundLabel setText:[NSString stringWithFormat:@"%.2f", [gameStatistics pointsPerRoundForPlayer:playerOne]]];
+    [playerTwoPointsPerRoundLabel setText:[NSString stringWithFormat:@"%.2f", [gameStatistics pointsPerRoundForPlayer:playerTwo]]];
+    
+    [playerOneTwentiesLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics twentiesForPlayer:playerOne]]];
+    [playerTwoTwentiesLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics twentiesForPlayer:playerTwo]]];
+    
+    [playerOneFifteensLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics fifteensForPlayer:playerOne]]];
+    [playerTwoFifteensLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics fifteensForPlayer:playerTwo]]];
+    
+    [playerOneTensLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics tensForPlayer:playerOne]]];
+    [playerTwoTensLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics tensForPlayer:playerTwo]]];
+    
+    [playerOneFivesLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics fivesForPlayer:playerOne]]];
+    [playerTwoFivesLabel setText:[NSString stringWithFormat:@"%d", [gameStatistics fivesForPlayer:playerTwo]]];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
@@ -55,15 +116,35 @@ const int ALERT_VIEW_DELETE_BUTTON_INDEX = 1;
         // Remove the game from the app delegate's loaded games.
         AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
         [[appDelegate games] removeObject:game];
-
+        
         // Delete the game.
         [CoreDataUtilities deleteEntity:game];
-
+        
         // Pop to the Players & Games screen.
         [[self navigationController] popViewControllerAnimated:YES];
     } else {
         // Unknown button! Dismiss the alert.
     }
+}
+
+- (void)viewDidUnload {
+    [self setPlayerOneLabel:nil];
+    [self setPlayerTwoLabel:nil];
+    [self setPlayerOneScoreLabel:nil];
+    [self setPlayerTwoScoreLabel:nil];
+    [self setPlayerOneRoundsWonLabel:nil];
+    [self setPlayerTwoRoundsWonLabel:nil];
+    [self setPlayerOnePointsPerRoundLabel:nil];
+    [self setPlayerTwoPointsPerRoundLabel:nil];
+    [self setPlayerOneTwentiesLabel:nil];
+    [self setPlayerTwoTwentiesLabel:nil];
+    [self setPlayerOneFifteensLabel:nil];
+    [self setPlayerTwoFifteensLabel:nil];
+    [self setPlayerOneTensLabel:nil];
+    [self setPlayerTwoTensLabel:nil];
+    [self setPlayerOneFivesLabel:nil];
+    [self setPlayerTwoFivesLabel:nil];
+    [super viewDidUnload];
 }
 
 @end

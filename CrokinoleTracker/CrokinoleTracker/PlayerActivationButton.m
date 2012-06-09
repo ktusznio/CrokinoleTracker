e9ff478fc04eaa7f2178196ee9840f9d9fead7f2
Max Woghiren <max.woghiren@gmail.com>
2012-06-09 16:45:35 -0400

    Got rid of some whitespace.

--- CrokinoleTracker/CrokinoleTracker/PlayerActivationButton.m
+++ CrokinoleTracker/CrokinoleTracker/PlayerActivationButton.m
@@ -29,7 +29,7 @@
         [[self titleLabel] setTextAlignment:UITextAlignmentCenter];
         [[self titleLabel] setFont:[UIFont fontWithName:@"Helvetica"
                                                    size:8]];
-        
+
         // Set up the handler.
         [self addTarget:self
                  action:@selector(onButtonTap)
@@ -42,11 +42,11 @@
 - (void)drawRect:(CGRect)rect {
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetLineWidth(context, 2.0);
-    
+
     // Draw the selection disc.
     CGContextSetFillColorWithColor(context, [self color].CGColor);
     CGContextFillEllipseInRect(context, rect);
-    
+
     // If this button is activated, make that clear.
     if ([self isActivated]) {
         [self setTitle:@"O" forState:UIControlStateNormal];

47c3ee2ae8eb63353e649cee7a6ae57be63c2385
Max Woghiren <max.woghiren@gmail.com>
2012-06-09 16:36:03 -0400

    Added a player activation button, used to select which player's discs are being placed.

---
+++ CrokinoleTracker/CrokinoleTracker/PlayerActivationButton.h
@@ -0,0 +1,29 @@
+//
+//  PlayerActivationButton.h
+//  CrokinoleTracker
+//
+//  Created by Kamil Tusznio and Maxwell Woghiren on 12-06-09.
+//  Copyright (c) 2012 Lost City Studios Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol PlayerActivationButtonDelegate
+
+- (void)buttonWasPressed:(id)sender;
+
+@end
+
+@interface PlayerActivationButton : UIButton
+
+@property (weak, nonatomic) id<PlayerActivationButtonDelegate> delegate;
+
+@property (nonatomic) BOOL isActivated;
+
+- (id)initWithFrame:(CGRect)frame
+           delegate:(id<PlayerActivationButtonDelegate>)aDelegate
+ initiallyActivated:(BOOL)anIsActivated
+              color:(UIColor *)color ;
+- (void)onButtonTap;
+
+@end
---
+++ CrokinoleTracker/CrokinoleTracker/PlayerActivationButton.m
@@ -0,0 +1,61 @@
+//
+//  PlayerActivationButton.m
+//  CrokinoleTracker
+//
+//  Created by Kamil Tusznio and Maxwell Woghiren on 12-06-09.
+//  Copyright (c) 2012 Lost City Studios Inc. All rights reserved.
+//
+
+#import "PlayerActivationButton.h"
+
+@implementation PlayerActivationButton
+
+@synthesize delegate;
+@synthesize isActivated;
+
+- (id)initWithFrame:(CGRect)frame
+           delegate:(id<PlayerActivationButtonDelegate>)aDelegate
+ initiallyActivated:(BOOL)anIsActivated
+              color:(UIColor *)color {
+    self = [super initWithFrame:frame];
+
+    if (self) {
+        [self setDelegate:aDelegate];
+        [self setIsActivated:anIsActivated];
+
+        // Style the button.
+        [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [[self titleLabel] setTextAlignment:UITextAlignmentCenter];
+        [[self titleLabel] setFont:[UIFont fontWithName:@"Helvetica"
+                                                   size:8]];
+        
+        // Set up the handler.
+        [self addTarget:self
+                 action:@selector(onButtonTap)
+       forControlEvents:UIControlEventTouchUpInside];
+    }
+
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 2.0);
+    
+    // Draw the selection disc.
+    CGContextSetFillColorWithColor(context, [UIColor yellowColor].CGColor);
+    CGContextFillEllipseInRect(context, rect);
+    
+    // If this button is activated, make that clear.
+    if ([self isActivated]) {
+        [self setTitle:@"O" forState:UIControlStateNormal];
+    } else {
+        [self setTitle:@"" forState:UIControlStateNormal];
+    }
+}
+
+- (void)onButtonTap {
+    [delegate buttonWasPressed:self];
+}
+
+@end
